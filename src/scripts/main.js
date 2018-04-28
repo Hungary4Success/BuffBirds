7a8025d8409c1c044cad75d9e5ceb58f46a14b13
András Geiszl <geiszla@gmail.com>
2018-04-28 15:06:56 +0100

    Project structure change

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,19 @@
 'use strict';
 
+let background;
+
 function init() {
   console.log('Script started!');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background = loadImage('http://www.guibingzhuche.com/data/out/173/1384241.png');
 }
 
 function draw() {
   clear();
+  background(background);
   ellipse(mouseX, mouseY, 50, 50);
 }
 
