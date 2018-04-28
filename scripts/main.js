3ffe9d68aa9840f790025d18ce9a5c857e31566f
Gyorgy Rethy <rethygy0410@gmail.com>
2018-04-28 16:05:30 +0100

    single bird flying model

--- scripts/main.js
+++ scripts/main.js
@@ -4,21 +4,54 @@ function init() {
   console.log('Script started!');
 }
 
-let background;
+var background;
+let birdPositionsX;
+let birdPositionsY;
+let birdVelocitiesX;
+let birdVelocitiesY;
+let birdVelocityXMean;
+let birdVelocityXSTD;
+let birdVelocityYMean;
+let birdVelocityYSTD;
 
 function setup() {
   createCanvas(windowWidth / 2, windowHeight / 2);
   background = loadImage('http://www.guibingzhuche.com/data/out/173/1384241.png');
+  birdVelocityXMean = 0.2;
+  birdVelocityXSTD = birdVelocityXMean - 0.00001;
+  birdVelocityYMean = 0;
+  birdVelocityYSTD = 3;
+  birdPositionsX = [-10];
+  birdPositionsY = [randomGaussian(height / 2, height / 4)];
+  birdVelocitiesX = [randomGaussian(birdVelocityXMean, birdVelocityXSTD)];
+  birdVelocitiesY = [randomGaussian(birdVelocityYMean, birdVelocityYSTD)];
+  console.log(birdPositionsY[0]);
 }
+
 
 function draw() {
   // put code here that needs to run at every image update
   clear();
   let radius = 50;
-  let randomY = randomGaussian(displayHeight / 2, displayHeight / 2);
-  ellipse(50, randomY, radius, radius);
-  background(background);
-  ellipse(50, 50, 50, 50);
+  ellipse(birdPositionsX[0], birdPositionsY[0], radius);
+
+  birdPositionsX[0] += birdVelocitiesX[0];
+  birdPositionsY[0] += birdVelocitiesY[0];
+
+  birdVelocitiesX[0] += randomGaussian(birdVelocityXMean, birdVelocityXSTD);
+  birdVelocitiesY[0] += randomGaussian(birdVelocityYMean, birdVelocityYSTD);
+
+  if (birdPositionsX[0] > width)
+  {
+    birdPositionsX[0] = -10;
+    birdVelocitiesX[0] = 0;
+    birdVelocitiesY[0] = 0;
+  }
+  if (birdPositionsY[0] > height - (radius / 2))
+    birdPositionsY[0] = height - (radius / 2);
+  else if (birdPositionsY[0] < radius / 2)
+    birdPositionsY[0] = radius / 2;
+  
 }
 
 function windowResized() {
