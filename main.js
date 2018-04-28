acc018870fea3d02ecfa3f0c8f0de6394827315b
Laszlo Nagy <laszlo.nagy@student.manchester.ac.uk>
2018-04-28 14:46:51 +0100

    added crosshair

--- main.js
+++ main.js
@@ -3,3 +3,12 @@
 function init() {
   console.log('Script started!');
 }
+
+function setup() {
+  createCanvas(1920, 1080);
+}
+
+function draw() {
+  clear();
+  ellipse(mouseX, mouseY, 50, 50);
+}
